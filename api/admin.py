5ca1a2f056c74299ead238f2fad5668a42d6967e
Qinchengqiang <steven.qin90@gmail.com>
2022-03-22 18:50:46 +1100

    update model field

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Person, Address
 from django import forms
-from django.forms import Textarea
+from django.forms import Textarea, ChoiceField
 
 
 class PersonAdminForm(forms.ModelForm):
@@ -20,8 +20,7 @@ class AddressAdminForm(forms.ModelForm):
         exclude = ()
         widgets = {
             'street': Textarea(attrs={'cols': '80', 'rows': '3'}),
-            'city': Textarea(attrs={'cols': '40', 'rows': '1'}),
-            'state': Textarea(attrs={'cols': '40', 'rows': '1'})
+            'city': Textarea(attrs={'cols': '40', 'rows': '1'})
         }
 
 
--- api/models.py
+++ api/models.py
@@ -1,12 +1,23 @@
 from django.db import models
+# from enum import Enum
+from django.utils.translation import gettext_lazy as _
 
 
 class Address(models.Model):
+    NSW = 'NSW'
+    VIC = 'VIC'
+    QLD = 'QLD'
+    StateChoice = [
+        (NSW, 'NSW'),
+        (VIC, 'VIC'),
+        (QLD, 'QLD'),
+    ]
+
     id = models.AutoField(primary_key=True)  # primary key
     number = models.IntegerField(null=False, blank=False)
     street = models.CharField(max_length=500, null=False, blank=False)
     city = models.CharField(max_length=100, null=False, blank=False)
-    state = models.CharField(max_length=100, null=True, blank=True)
+    state = models.CharField(max_length=100, choices=StateChoice, default=NSW)
 
     def __str__(self):
         return f"{self.number}, {self.street}, {self.city}, {self.state}"
